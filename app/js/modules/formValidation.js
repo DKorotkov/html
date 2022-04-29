0114aa9271134f0e1a641df108e92410c529865a
Danila Korotkov <danila.korotkov@gmail.com>
2022-04-29 19:38:37 +0300

    Обновли js валидации формы

--- app/js/modules/formValidation.js
+++ app/js/modules/formValidation.js
@@ -7,11 +7,11 @@ class FormValid {
 
    static init() {
       const defaultOptions = {
-         errorClassName: "error",
+         errorClassName: "appointment-form__error",
          inputClassName: "input",
          invalidClassName: "invalid",
          errors: {
-            required: "Это поле обязательное",
+            required: "Заполните обязательное поле",
             email: "Пожалуйста, введите правильный email",
             type: "Не соответствует формату поля",
             pattern: "Не соответствует формату поля",
@@ -27,10 +27,7 @@ class FormValid {
       this.options = Object.assign(defaultOptions, this.constructorOptions);
 
       this.forms = document.querySelectorAll("form");
-      if (!this.forms) {
-         console.error("Форма на странице не найдена");
-         return;
-      }
+      if (!this.forms) console.error("Форма на странице не найдена");
 
       this.forms.forEach((form) => {
          // form.setAttribute("novalidate", "");
@@ -47,7 +44,7 @@ class FormValid {
          });
 
          form.addEventListener("submit", (e) => {
-            this.checkValid(e, inputs);
+            this.checkValid(e);
          });
       });
    }
@@ -61,7 +58,6 @@ class FormValid {
 
       if (!input.validity.valid) {
          // Проверка на обязательное поле
-         console.log(input.validity);
          if (input.validity.valueMissing) {
             this.setInvalid(input, this.options.errors.required);
             return error;
@@ -113,28 +109,6 @@ class FormValid {
          }
       }
 
-      // Проверка на required
-      // if (input.hasAttribute("required") && input.value == "") {
-      //    this.setInvalid(input, this.options.errors.required);
-      //    return error;
-      // }
-      // // Проверка по паттерну
-      // if (pattern) {
-      //    const ptr = new RegExp(pattern, "g");
-      //    const patternResult = input.value.match(ptr);
-      //    if (!patternResult) {
-      //       this.setInvalid(input, this.options.errors.pattern);
-      //       return error;
-      //    }
-      // }
-      // // Проверки для поля "почта"
-      // if (inputType === "email")
-      //    if (!input.value.includes("@")) {
-      //       // заменить данную проверку на паттерн, который будет браться из html данного input
-      //       this.setInvalid(input, this.options.errors.email);
-      //       return error;
-      //    }
-
       input.classList.remove(`${this.options.inputClassName}--${this.options.invalidClassName}`);
       error.innerHTML = "";
    }
@@ -147,9 +121,12 @@ class FormValid {
    }
 
    // Проверят поля перед отправкой, в случае, если по ним не проходили или не заметили ошибку
-   static checkValid(e, inputs) {
+   static checkValid(e) {
       let errorElements = new Array();
-      e.preventDefault();
+      const form = typeof e.currentTarget === "object" ? e.currentTarget : e;
+      const inputs = form.querySelectorAll("input");
+
+      if (typeof e.currentTarget === "object") e.preventDefault();
       inputs.forEach((input) => {
          errorElements.push(this.validate(input));
       });
@@ -159,8 +136,8 @@ class FormValid {
       });
 
       if (errorElements.length === 0) {
-         e.target.closest("form").submit();
-         return;
+         form.submit();
+         return true;
       }
 
       errorElements[0].previousSibling.focus();
@@ -168,5 +145,14 @@ class FormValid {
 }
 
 // Вынести этот модуль ввиде отдельного проекта, вывести на гитхаб с описанием
-// Кнопка если не субмит
-// ввиде заданного экземпляра как работать будет
+
+// -----------Пример работы с кнопкой формы отправки без submit----------------
+// const submitBtns = document.querySelectorAll(".appointment-form__submit");
+// submitBtns.forEach((submitBtn) => {
+//    submitBtn.addEventListener("click", (e) => {
+//       const thisForm = e.currentTarget.closest("form");
+//       if (FormValid.checkValid(thisForm)) {
+//          console.log("Отправляем форму");
+//       }
+//    });
+// });
