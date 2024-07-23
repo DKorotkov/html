be0965313ab7e869f602fd3f461e5df78247e7ba
Danila Korotkov <danila.korotkov@gmail.com>
2024-07-23 18:54:38 +0300

    Обновил страницу Элементов

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,47 +1,47 @@
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
-import autoprefixer from "gulp-autoprefixer";
-import group_media from "gulp-group-css-media-queries";
-import GulpCleanCss from "gulp-clean-css";
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import autoprefixer from 'gulp-autoprefixer'
+import group_media from 'gulp-group-css-media-queries'
+import GulpCleanCss from 'gulp-clean-css'
 
 // import webpCss from "gulp-webpcss";
 
-const sass = gulpSass(dartSass);
+const sass = gulpSass(dartSass)
 
 export const scss = () => {
-   return (
-      gl.gulp
-         .src(gl.path.app.scss)
-         .pipe(gl.plugins.newer(gl.path.dist.css))
-         .pipe(
-            gl.plugins.rename({
-               extname: ".min.css",
-            })
-         )
-         .pipe(gl.plugins.gulpIf(gl.isDev, gl.plugins.sourcemaps.init()))
-         .pipe(gl.plugins.replace(/@img\//g, "img/"))
-         .pipe(
-            sass({
-               outputStyle: "compressed",
-            }).on("error", sass.logError)
-         )
-         .pipe(
-            autoprefixer({
-               grid: true,
-               overrideBrowserslist: ["last 5 versions"],
-               cascade: true,
-            })
-         )
-         .pipe(gl.plugins.gulpIf(gl.isBuild, group_media()))
-         // .pipe(
-         //    webpCss({
-         //       webpClass: ".webp",
-         //       noWebpClass: ".no-webp",
-         //    })
-         // )
-         .pipe(gl.plugins.gulpIf(gl.isBuild, GulpCleanCss()))
-         .pipe(gl.plugins.gulpIf(gl.isDev, gl.plugins.sourcemaps.write("./maps")))
-         .pipe(gl.gulp.dest(gl.path.dist.css))
-         .pipe(gl.plugins.browserSync.stream())
-   );
-};
+	return (
+		gl.gulp
+			.src(gl.path.app.scss)
+			.pipe(gl.plugins.newer(gl.path.dist.css))
+			.pipe(
+				gl.plugins.rename({
+					extname: '.min.css',
+				})
+			)
+			.pipe(gl.plugins.gulpIf(gl.isDev, gl.plugins.sourcemaps.init()))
+			.pipe(gl.plugins.replace(/@img\//g, 'img/'))
+			.pipe(
+				sass({
+					outputStyle: 'compressed',
+				}).on('error', sass.logError)
+			)
+			.pipe(
+				autoprefixer({
+					grid: 'autoplace',
+					overrideBrowserslist: ['last 5 versions'],
+					cascade: true,
+				})
+			)
+			.pipe(gl.plugins.gulpIf(gl.isBuild, group_media()))
+			// .pipe(
+			//    webpCss({
+			//       webpClass: ".webp",
+			//       noWebpClass: ".no-webp",
+			//    })
+			// )
+			.pipe(gl.plugins.gulpIf(gl.isBuild, GulpCleanCss()))
+			.pipe(gl.plugins.gulpIf(gl.isDev, gl.plugins.sourcemaps.write('./maps')))
+			.pipe(gl.gulp.dest(gl.path.dist.css))
+			.pipe(gl.plugins.browserSync.stream())
+	)
+}
